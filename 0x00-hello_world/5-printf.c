262a242a375f17407b00489dd1ee40462ab5f1a6
keenah27 <makinatendai441@gmail.com>
2023-07-13 18:45:53 -0700

    adding description

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * description : printf function script
+ *main = main entry point
+ *return: 0 success
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
+
